3427046f4dd0a1bb68451c6bedddbace7470a82b
Cool BRG <budhram.gurung01@gmail.com>
2020-10-12 22:15:20 +0530

    Added sinatra

---
+++ sinatra/hello_sinatra.rb
@@ -0,0 +1,7 @@
+require 'sinatra'
+
+get '/' do
+  '<h1>Hello Nepal!</h1>
+  <h2>I am <a href="http://sinatrarb.com/">Sinatra</a>.</h2>
+  <h3>I can help you create Web application quickly.</h3>'
+end
